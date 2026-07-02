da48ca45fc49983987e1037ce65545bb0295460d
Ubuntu <281125600+GH3stu@users.noreply.github.com>
2026-07-02 02:35:23 +0000

    mk_top_page_2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get "static_pages/top"
-  get "static_pages/about"
+  root "homes#top" 
+  get "about", to: "homes#about" 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
