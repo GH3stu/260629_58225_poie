773381c605b2ed9f7ae44d19e7cb890164222757
Ubuntu <281125600+GH3stu@users.noreply.github.com>
2026-07-05 10:16:38 +0000

    add_admin_2

--- test/controllers/admin/homes_controller_test.rb
+++ test/controllers/admin/homes_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class Admin::HomesControllerTest < ActionDispatch::IntegrationTest
   test "should get admin" do
-    get admin_homes_admin_path
+    get admin_root_path
     assert_response :success
   end
 end
--- test/controllers/admin/posts_controller_test.rb
+++ test/controllers/admin/posts_controller_test.rb
@@ -1,21 +1,36 @@
 require "test_helper"
 
 class Admin::PostsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin = Admin.create!(
+      email_address: "admin@example.com",
+      password: "password"
+    )
+
+    # 管理者ログイン状態を作る
+    post admin_login_path, params: {
+      email_address: @admin.email_address,
+      password: "password"
+    }
+
+    @post = posts(:one)
+  end
+
   test "should get index" do
     get admin_posts_path
     assert_response :success
   end
 
   test "should get show" do
-    post = posts(:one)
-    get admin_post_path(post)
+    get admin_post_path(@post)
     assert_response :success
   end
 
   test "should get destroy" do
-    post = posts(:one)
-    delete admin_post_path(post)
+    delete admin_post_path(@post)
     assert_response :redirect
+    assert_redirected_to admin_posts_path
   end
 end
+
 
