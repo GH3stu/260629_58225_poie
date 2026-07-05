29728a0f050df3f2f9ec77f86f233b23f1af262a
Ubuntu <281125600+GH3stu@users.noreply.github.com>
2026-07-05 07:17:38 +0000

    add_tests_3

--- db/migrate/20260705043421_create_admins.rb
+++ db/migrate/20260705043421_create_admins.rb
@@ -1,10 +1,16 @@
-class CreateAdmins < ActiveRecord::Migration[8.0]
-  def change
-    create_table :admins do |t|
-      t.string :email_address
-      t.string :password_digest
-
-      t.timestamps
+class Admin::SessionsController < ApplicationController
+  def create
+    admin = Admin.find_by(email_address: params[:email])   # 修正ポイント
+    if admin&.authenticate(params[:password])
+      session[:admin_id] = admin.id
+      redirect_to admin_posts_path
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    reset_session
+    redirect_to admin_login_path
   end
 end
