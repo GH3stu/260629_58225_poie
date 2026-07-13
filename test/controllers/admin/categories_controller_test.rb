96d4cfe2bbf5eed5926617daf0ef49683f8a9303
Ubuntu <281125600+GH3stu@users.noreply.github.com>
2026-07-13 07:26:14 +0000

    mk_category

--- test/controllers/admin/categories_controller_test.rb
+++ test/controllers/admin/categories_controller_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class Admin::CategoriesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get admin_categories_index_path
+    get admin_categories_path
     assert_response :success
   end
 
   test "should get new" do
-    get admin_categories_new_path
+    get new_admin_category_path
     assert_response :success
   end
 end
