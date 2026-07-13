84bb8cc7a8b60add2d65e3488f7e24d2b97d3f27
Ubuntu <281125600+GH3stu@users.noreply.github.com>
2026-07-13 07:37:46 +0000

    mk_category

--- test/controllers/category_posts_controller_test.rb
+++ test/controllers/category_posts_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class CategoryPostsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get category_category_posts_path(1)
+    get category_posts_path
     assert_response :success
   end
 end
