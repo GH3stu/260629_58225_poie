85bb51ec9fb24b738753c412f649eb45fff7b5e0
Ubuntu <281125600+GH3stu@users.noreply.github.com>
2026-07-13 07:21:19 +0000

    mk_category

--- test/controllers/admin/categories_controller_test.rb
+++ test/controllers/admin/categories_controller_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class Admin::CategoriesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get admin_categories_index_url
+    get admin_categories_index_path
     assert_response :success
   end
 
   test "should get new" do
-    get admin_categories_new_url
+    get admin_categories_new_path
     assert_response :success
   end
 end
--- test/controllers/category_posts_controller_test.rb
+++ test/controllers/category_posts_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class CategoryPostsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get category_posts_index_url
+    get category_posts_index_path
     assert_response :success
   end
 end
