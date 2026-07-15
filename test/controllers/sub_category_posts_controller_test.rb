2c34d4040811f34054f87da3540e7ae892aa195f
Ubuntu <281125600+GH3stu@users.noreply.github.com>
2026-07-15 04:45:36 +0000

    add_posts_3

--- test/controllers/sub_category_posts_controller_test.rb
+++ test/controllers/sub_category_posts_controller_test.rb
@@ -2,7 +2,11 @@ require "test_helper"
 
 class SubCategoryPostsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get sub_category_posts_index_url
+    # SubCategory は category に属するだけ
+    category = Category.create!(name: "Test Category", purpose: Purpose.create!(name: "Test Purpose"))
+    sub_category = SubCategory.create!(name: "Test SubCategory", category: category)
+
+    get sub_category_posts_path(sub_category)
     assert_response :success
   end
 end
